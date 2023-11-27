53621be5ce9649e0ee4221d4a275a82bb9229f7c
root <root@ubuntu2004.linuxvmimages.local>
2023-11-27 10:29:45 -0500

    puuprt

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,19 @@
-#!/usr/bin/env bash
-# using puppet for ssh  configuration
+# Puppet manifest file: ssh_config.pp
+include stdlib
 
 file { '/etc/ssh/ssh_config':
- ensure => present,
+  ensure => present,
 }
 
-file_line { "Turn off passwd auth':
- path => '/etc/ssh/ssh_config',
- line => 'PasswordAuthentication no',
- match => '#PasswordAuthentication',
- }
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+  match => '#PasswordAuthentication',
+}
 
-file_line { "Declare identity file':
- path => '/etc/ssh/ssh_config',
- line => 'IdentityFile ~/.ssh/school',
- match => '#IdentityFile',
- }
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '#IdentityFile',
+}
+
