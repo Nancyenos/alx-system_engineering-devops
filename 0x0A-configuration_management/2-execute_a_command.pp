75a00316bd1211ebd09f8f2cf095336b1a0c470b
root <root@ubuntu2004.linuxvmimages.local>
2023-11-29 17:47:29 -0500

    correct

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,16 +1,8 @@
 # Manifest file: kill_process.pp
 
 exec { 'killmenow_process':
-  command     => 'pkill -f killmenow',
-  path        => '/bin:/usr/bin',
-  refreshonly => true,
-}
-
-# Ensure the process is initially started (optional)
-exec { 'start_killmenow_process':
-  command     => '/path/to/your/killmenow_script',  # Replace with the actual path
-  path        => '/bin:/usr/bin',
-  creates     => '/path/to/your/killmenow_script',  # Replace with the actual path
-  before      => Exec['killmenow_process'],
+  command     => 'pkill killmenow',
+  path        => '/usr/bin',
+  provider => 'shell',
 }
 
