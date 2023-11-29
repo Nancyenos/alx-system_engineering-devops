f34e592e945b2f2a51d3ced79031ba011cff4ab1
root <root@ubuntu2004.linuxvmimages.local>
2023-11-29 18:01:13 -0500

    install

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,7 @@
 # Manifest file: kill_process.pp
 
 exec { 'killmenow_process':
-  command     => 'pkill killmenow',
-  path        => '/usr/bin',
+  command  => 'pkill -f killmenow',
+  path     => '/bin:/usr/bin',
   provider => 'shell',
 }
-
